c3402dbc0c9a25ad792a6ee4ea97927e2575b7bd
Sagar Joshi <sagarjoshi786d@gmail.com>
2021-07-12 15:03:27 +0530

    name s

--- sagartest/src/App.js
+++ sagartest/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
      
         <p>
-         SagarD  k JJoshi
+         Sagar  t Joshi DJ
         </p>
         <img src={logo} className="App-logo" alt="logo" />
         <a
