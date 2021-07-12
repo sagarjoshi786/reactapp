3b955ac333d2d4f18b587ff14185dd0f312b505a
Sagar Joshi <sagarjoshi786d@gmail.com>
2021-07-12 16:05:00 +0530

    name changed to ravi

--- sagartest/src/App.js
+++ sagartest/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
      
         <p>
-         Sagar  t Joshi DJ
+         Ravi Ruparel
         </p>
         <img src={logo} className="App-logo" alt="logo" />
         <a
