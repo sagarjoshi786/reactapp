e1c640d93a31b1c5f8f257df175732424e6a725d
Sagar Joshi <sagarjoshi786d@gmail.com>
2021-07-12 15:55:27 +0530

    Name change to chirag

--- sagartest/src/App.js
+++ sagartest/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
      
         <p>
-         Sagar  t Joshi DJ
+         Chirag Raval
         </p>
         <img src={logo} className="App-logo" alt="logo" />
         <a
